9c935d606374d4df590b56d1998bf4faeb3cdd6f
Nicklas Thor <nicke.tor.86@gmail.com>
2021-09-29 17:17:21 +0200

    Add function get_user_from_list and get_user_int_input.

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
 import platform
-import psutil
+# # Temp comment for not haveing psutil
+# import psutil 
 import math
+
+def get_user_int_input(question):
+    # Get input from user called int_input as int based on str question. If input is not int reppet until it is an int.
+    while True:
+        try:
+            int_input = int(input(question))
+            break
+        except ValueError:
+            print('You have to chose a integer')
+            pass
+
+    return int_input
+
+def user_chose_from_list(list):
+    # take a list with strings and ask user to chose one and return the int chose_index that is index for that chose in list.
+    chose_text = ''
+    chose_counter = 1
+    chose_number = 0
+
+    for text in list:
+        chose_text += f'{chose_counter}: {text}\n'
+        chose_counter += 1
+        
+    while True:
+        chose_number = get_user_int_input(chose_text)
+        
+        # check if chose_number is one of the list choices
+        if chose_number <= len(list) and chose_number > 0:
+            break
+        else:
+            print('You have to chose one of this choices')
+
+    # chose_index is index chosen based on chose_number in list
+    chose_index = chose_number - 1
+    return chose_index
+
+# TEST CODE
+print(user_chose_from_list(('tetetete', 'dggddgdgdgdggdg')))
 
 # thor main function
 def dragon_lair():
@@ -22,4 +61,6 @@ def GetPCInfo():
     Result += "\nPython Version: "
     Result += platform.python_version()
     return Result
-print(GetPCInfo())
+
+# # temp comments to not run
+# print(GetPCInfo()) 
