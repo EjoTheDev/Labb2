5fdc9969975fee9c6f0238c3e9ba2d9755af9df1
ninos77 <ninos_zaiya@hotmail.com>
2021-10-23 17:33:44 +0200

    Added another choice exite

--- ninos.py
+++ ninos.py
@@ -14,6 +14,7 @@ def show_options():
     print('3.Volume of an Sphere')
     print('4.Area of an Circle')
     print('5.Volume of an Cylinder')
+    print('6.Exite')
     print('=================================')
 
 
@@ -24,7 +25,7 @@ def show_thnks():
 
     
 PI = math.pi
-options = [1,2,3,4,5]
+options = [1,2,3,4,5,6]
 
 
 
@@ -35,8 +36,8 @@ def is_options(value):
             return True
         else:
             return False
-    except:
-        return False
+    except ValueError:
+      return False
 
 
 def cal_vol_cube(length):
@@ -58,8 +59,11 @@ def cal_area_circle(r):
 def cal_vol_cylinder(r,h):
   return PI * (r**2) * h
 
+def exite_app():
+  print("The Program Has Been Terminated By The User")
+  
 
-dic_shape_form = {1:cal_vol_cube,2:cal_area_square,3:cal_vol_sphere,4:cal_area_circle,5:cal_vol_cylinder}
+dic_shape_form = {1:cal_vol_cube,2:cal_area_square,3:cal_vol_sphere,4:cal_area_circle,5:cal_vol_cylinder,6:exite_app}
 
 
 
@@ -69,40 +73,48 @@ def start_cal():
     show_options()
     choice = int(input("Please make your choice: "))
     while is_options(choice) == False:
-      choice = int(input("Please make your right choice between (1-5): "))
-    unit = input("Which unit you want to use in this calculation? ")
+      choice = int(input(f"Please make your right choice between ({str(options[0])}-{str(options[-1])}): "))
     if choice in dic_shape_form.keys():
       if choice == 1:
+        unit = input("Which unit you want to use in this calculation? ")
         a = int(input("Enter the side length of Cube: "))
         print(f'Volume of an equilateral Cube is: {round(dic_shape_form[choice](a),2)}{unit}')
         ask = input('Want to make anothe calculate for another shape y/n: ').lower()
         if ask == 'n':
           break
       elif choice == 2:
+        unit = input("Which unit you want to use in this calculation? ")
         r = int(input("Enter the length of Square: "))
         print(f'Area of an Square is: {round(dic_shape_form[choice](r),2)}{unit}')
         ask = input('Want to make anothe calculate for another shape y/n: ').lower()
         if ask == 'n':
           break
       elif choice == 3:
+        unit = input("Which unit you want to use in this calculation? ")
         l = int(input("Enter the radius of Sphere: "))
         print(f'Volume of an Sphere is: {round(dic_shape_form[choice](l),2)}{unit}')
         ask = input('Want to make anothe calculate for another shape y/n: ').lower()
         if ask == 'n':
           break
       elif choice == 4:
+        unit = input("Which unit you want to use in this calculation? ")
         r = int(input("Enter the radius of Circle : "))
         print(f'Area of an Circle is: {round(dic_shape_form[choice](r),2)}{unit}')
         ask = input('Want to make anothe calculate for another shape y/n: ').lower()
         if ask == 'n':
           break
       elif choice == 5:
+        unit = input("Which unit you want to use in this calculation? ")
         h = int(input("Enter the height of Cylinder : "))
         r = int(input("Enter the radius of Cylinder : "))
         print(f'Volume of an Cylinder is: {round(dic_shape_form[choice](h,r),2)}{unit}')
         ask = input('Want to make anothe calculate for another shape y/n: ').lower()
         if ask == 'n':
           break
+      elif choice == 6:
+        print("\n")
+        dic_shape_form[choice]()
+        break
       else:
         print("Please, chose the corect option!!")  
   show_thnks()
